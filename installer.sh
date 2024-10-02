80af528f5f5b6d8d4122e7c8108d9f2340eb6512
hwac121 <hwac121@protonmail.com>
2024-10-01 23:51:05 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# TITLE: Apache2Control
+# AUTHOR: Majik Cat Security
+# DATE: July 21, 2019
+# GITHUB: https://github.com/hwac121
+
 cp myweb.sh /bin/myweb
 chmod +x /bin/myweb
 chmod +x myweb.sh
